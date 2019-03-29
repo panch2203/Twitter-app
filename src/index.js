c8ec7c3c76d623e434a8690e6289d029ac55b612
Francisco <panch2203@gmail.com>
2019-03-29 04:55:55 -0600

    Cambios del fetch y componentes padre

--- src/index.js
+++ src/index.js
@@ -5,33 +5,84 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-class Tweet extends React.Component {
+class TweetMain extends React.Component {
 
 	constructor(props){
 		super(props);
 		this.state = {
 	      value: [],
-	      textvalue : "",	      
+	      error: null,
+	      textvalue : "",	     
+	      isLoaded: false,
+	      hour: "2019-03-27T05:26:56.828Z",
 	    }
     this.agregarTweet = this.agregarTweet.bind(this)
-    this.handleChange = this.handleChange.bind(this)    
+    this.handleChange = this.handleChange.bind(this)      
+	}
+
+	componentDidMount(){
+		fetch("https://still-garden-88285.herokuapp.com/draft_tweets")
+			.then(res => res.json())
+			.then(
+				(result) => {
+				let newTweets = this.state.value.slice();
+					this.setState({
+						isLoaded: true,
+						value: newTweets.concat(result.draft_tweets) 
+					});
+				},
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error:error
+					});
+				}
+			)
 	}
 
 	handleChange(e) {
 	    this.setState({
 	      textvalue:e.target.value
 	    })
-	}
+	    let hourTweet = new Date().toLocaleTimeString();	
+		console.log(hourTweet);	
+		this.setState({hour: hourTweet});
+	}	
 	
 	agregarTweet() {
-	    this.state.value.push(this.state.textvalue)
+		let tweet = {
+			avatar : "https://upload.wikimedia.org/wikipedia/commons/5/58/Shiba_inu_taiki.jpg",
+			user_name : "FranciscoDelgado",
+			description : this.state.textvalue,
+			created_at : this.state.hour,
+		}
+	    this.state.value.push(tweet)
 	    this.setState(
 	      this.state
 	    )	    
 	} 
+
+
 	//Link para gatito img https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9a3NmbeTOIkEcIHoGyWabEzCb7VIgNakbPtIiiN_RmnrSsZq1
 	render(){
-		let { value } = this.state;
+		let { value, error, isLoaded } = this.state;
+		let content;
+		console.log(value)
+
+		if(error){			
+			content = <div>Error: {error.message}</div>		
+		}
+		else if(!isLoaded){
+			content = <div>Loading...</div>
+		}
+
+		else content = value.map((v, index) => {
+			          return <div className="perfilTweet" key={index}>
+			          {console.log(v)}
+			          <TweetBody foto={v.avatar} nom={v.user_name} hour={v.created_at}></TweetBody><h1 className="texto">{v.description}</h1>
+			          </div>			          
+		})
+
 		return(
 			<div className="cuerpo">
 				<div className="perfil">
@@ -40,60 +91,61 @@ class Tweet extends React.Component {
 					<p className="textobold">FranciscoDelgado :)</p>
 					<p className="textobold">@panch2203</p>
 				</div>
-				<div>	
-					<label className="texto">Publica tu tweet</label>
-					<input type="text" name="tweet" onChange={ this.handleChange }></input>			    			   				  
-					<button className="btnsubmit" onClick={ this.agregarTweet } className="texto">Publicar</button>
-				</div>
-				<div>
-					{value.map((v, index) => {
-			          return <div className="perfilTweet" key={index}><TweetBody></TweetBody><h1 className="texto">{v}</h1></div>			          
-			        })}
-				</div>
+				<Tweet value={value} error={error} isLoaded={isLoaded} handleChange={this.handleChange} agregarTweet={this.agregarTweet}></Tweet>				
 			</div>
 		);
 	}
 }
 
+class Tweet extends React.Component{
 
-class TweetBody extends React.Component{
-	constructor(props){
-		super(props);		
-		this.state = {
-			hour: '10:30:2',
+	render(){
+		let content;		
+
+		if(this.props.error){			
+			content = <div>Error: {this.props.error.message}</div>		
 		}
-		this.setHour = this.setHour.bind(this)
+		else if(!this.props.isLoaded){
+			content = <div>Loading...</div>
+		}
+		else content = this.props.value.map((v, index) => {
+			          return <div className="perfilTweet" key={index}>
+			          {console.log(v)}
+			          <TweetBody foto={v.avatar} nom={v.user_name} hour={v.created_at}></TweetBody><h1 className="texto">{v.description}</h1>
+			          </div>			          
+		})
 
+		return(
+			<div>
+				<div>	
+					<label className="texto">Publica tu tweet</label>
+					<input type="text" name="tweet" onChange={ this.props.handleChange }></input>		    			   				  
+					<button className="btnsubmit" onClick={ this.props.agregarTweet } className="texto">Publicar</button>
+				</div>
+				<div>
+					{content}
+				</div>
+			</div>
+		)
 	}
+}
 
-	setHour(){
-		let hourTweet = new Date().toLocaleTimeString();	
-		console.log(hourTweet);	
-		this.setState({hour: hourTweet});
-	}
+
+class TweetBody extends React.Component{	
 
 	render(){		
 		return(			
-			<div className="perfilTweet">{this.setHour}
-				<img className="kitten" src="https://upload.wikimedia.org/wikipedia/commons/5/58/Shiba_inu_taiki.jpg"></img> 
-				<p className="textobold">FranciscoDelgado</p>
-				<p className="textobold">{this.state.hour}</p>
+			<div className="perfilTweet">
+				<img className="kitten" src={this.props.foto}></img> 
+				<p className="textobold">{this.props.nom}</p>
+				<p className="textobold">{this.props.hour}</p>
 			</div>
 		);		
 	}
 }
 
-class Container extends React.Component {
-  render() {
-    return (
-      <Tweet 
-      /> 
-    )
-  }
-}
 
-
-ReactDOM.render(<Tweet />, document.getElementById('root'));
+ReactDOM.render(<TweetMain />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
